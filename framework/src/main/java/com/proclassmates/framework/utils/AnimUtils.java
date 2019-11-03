785a0653f74a763692478708cdd7ca3d2e0246c2
SimonWangChen <hrbwangchen90@163.com>
2019-11-03 10:04:02 +0800

    add anim utils

--- framework/src/main/java/com/proclassmates/framework/utils/AnimUtils.java
+++ framework/src/main/java/com/proclassmates/framework/utils/AnimUtils.java
@@ -1,5 +1,10 @@
 package com.proclassmates.framework.utils;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+
 /**
  * @author simon
  * @name imoocMeet
@@ -7,4 +12,18 @@ package com.proclassmates.framework.utils;
  * @time 2019-11-03 10:01
  */
 public class AnimUtils {
+
+    /**
+     * 旋转动画
+     * @param view
+     * @return
+     */
+    public static ObjectAnimator rotation(View view) {
+        ObjectAnimator mAnim = ObjectAnimator.ofFloat(view, "rotation", 0f, 360f);
+        mAnim.setDuration(2 * 1000);
+        mAnim.setRepeatMode(ValueAnimator.RESTART);
+        mAnim.setRepeatCount(ValueAnimator.INFINITE);
+        mAnim.setInterpolator(new LinearInterpolator());
+        return mAnim;
+    }
 }
